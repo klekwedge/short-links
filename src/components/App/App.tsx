c8ab7737d8b694e7fc5f9a7d9c28178ea643ac39
Andrey <antonov.andrey.10101@gmail.com>
2023-08-14 21:40:01 +0400

    feat: add Form

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,12 +3,14 @@ import Features from '../Features/Features';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import Hero from '../Hero/Hero';
+import Form from '../Form/Form';
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <Form />
       <Features />
       <CallToAction />
       <Footer />
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,18 +1,20 @@
+/* eslint-disable react/button-has-type */
 import cn from 'classnames';
 import classes from './Button.module.scss';
 
 interface ButtonProps {
   onClick?: () => void;
+  type: string;
   variant?: string;
   size?: string;
   children: React.ReactNode;
 }
 
-function Button({ onClick, variant = '', size = 'medium', children }: ButtonProps) {
+function Button({ onClick, variant = '', type, size = 'medium', children }: ButtonProps) {
   const mainCn = cn(classes.button, classes[size], classes[variant]);
 
   return (
-    <button className={mainCn} type='button' onClick={onClick}>
+    <button className={mainCn} type={type} onClick={onClick}>
       {children}
     </button>
   );
