6ef7b32f42fdfb4f61395f18195ddecaa13654f9
Andrey <antonov.andrey.10101@gmail.com>
2023-08-14 18:32:46 +0400

    feat: add Hero

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,18 +2,17 @@ import cn from 'classnames';
 import classes from './Button.module.scss';
 
 interface ButtonProps {
-  onClick: () => void;
-  variant: string;
+  onClick?: () => void;
+  variant?: string;
   size: string;
-  type: string;
   children: React.ReactNode;
 }
 
-function Button({ onClick, variant = '', size = 'medium', type = 'button', children }: ButtonProps) {
+function Button({ onClick, variant = '', size = 'medium', children }: ButtonProps) {
   const mainCn = cn(classes.button, classes[size], classes[variant]);
 
   return (
-    <button className={mainCn} type={type} onClick={onClick}>
+    <button className={mainCn} type='button' onClick={onClick}>
       {children}
     </button>
   );
---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,22 @@
+import Img from 'images/illustration-working.svg';
+import classes from './Hero.module.scss';
+import Button from '../Button/Button';
+
+function Hero() {
+  return (
+    <section className={`${classes.hero} container`}>
+      <div className={classes.imgContainer}>
+        <img src={Img} alt="hero" className={classes.img} />
+      </div>
+      <article className={classes.text}>
+        <h1 className={classes.title}>More than just shorter links</h1>
+        <p className={classes.description}>
+          Build your brand&apos;s recognition and get detailed insights on how your links are performing.
+        </p>
+        <Button size="large">Get Started</Button>
+      </article>
+    </section>
+  );
+}
+
+export default Hero;
